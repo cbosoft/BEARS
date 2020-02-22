bb0e5ee39bdf8a68342eb709e3363a1f7f624254
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-02-22 23:39:35 +0000

    Updated distance calc: finding time of collision

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,9 @@ std::string vec3_repr(const Vec &v)
 Vec vec3_cross(Vec u, Vec v)
 {
   return { 
-      (u[1]*v[2] - u[2]*v[1]),
-      (u[2]*v[0] - u[0]*v[2]),
-      (u[0]*v[1] - u[1]*v[0])
+      (u[1]*v[2]) - (u[2]*v[1]),
+      (u[2]*v[0]) - (u[0]*v[2]),
+      (u[0]*v[1]) - (u[1]*v[0])
   };
 }
 
@@ -31,7 +31,7 @@ Vec vec3_diff(const Vec &u, const Vec &v)
 
 double vec3_dot(const Vec &u, const Vec &v)
 {
-  return u[0]*v[0] + u[1]*v[1] + u[2]*v[2];
+  return (u[0]*v[0]) + (u[1]*v[1]) + (u[2]*v[2]);
 }
 
 Vec vec3_scalar_mult(double m, const Vec &v)
@@ -46,7 +46,7 @@ Vec vec3_add(const Vec &u, const Vec &v)
 
 double vec3_magnitude(Vec u)
 {
-  double asq = u[0]*u[0] + u[1]*u[1] + u[2]*u[2];
+  double asq = (u[0]*u[0]) + (u[1]*u[1]) + (u[2]*u[2]);
   return std::pow(asq, 0.5);
 }
 
@@ -77,12 +77,22 @@ bool lines_intersect(const Ball &a, const Ball &b)
   Vec p1 = a.position;
   Vec p2 = b.position;
 
+  std::cout << vec3_repr(v1) << " " << vec3_repr(v2) << std::endl;
   std::pair<Vec, Vec> close_points = get_points_of_closest_approach(p1, v1, p2, v2);
   Vec c1 = close_points.first;
   Vec c2 = close_points.second;
   double closest_distance = vec3_magnitude(vec3_diff(c1, c2));
-  std::cout << closest_distance << std::endl;
+  std::cout << vec3_repr(c1) << " " << vec3_repr(c2) << " " << closest_distance << std::endl;
   double thresh_distance = (a.diameter/2) + (b.diameter/2);
+
+  double d1 = vec3_magnitude(vec3_diff(c1, p1));
+  double d2 = vec3_magnitude(vec3_diff(c2, p2));
+
+  if (std::abs(d1 - d2) > thresh_distance)
+    return false; // balls will miss each other
+
+  double t1 = d1 / vec3_magnitude(v1);
+  double t2 = d2 / vec3_magnitude(v2);
 
   return closest_distance <= thresh_distance;
 }
@@ -95,9 +105,9 @@ int main(void)
       .diameter=1.0, 
       .mass=1.0, 
       .roughness=0.0, 
-      .position={-0.5, 0.5, 0.0},
+      .position={2, 0.0, 0.0},
       .orientation={0.0, 0.0, 0.0},
-      .velocity={ 1.0, 0.1, 0.0},
+      .velocity={-1.0, 0.0, 0.0},
       .angular_velocity={0.0, 0.0, 0.0},
       .force={0.0, 0.0, 0.0},
       .torque={0.0, 0.0, 0.0}});
@@ -106,9 +116,9 @@ int main(void)
       .diameter=1.0, 
       .mass=1.0, 
       .roughness=0.0, 
-      .position={ 0.5, -0.3, 0.0},
+      .position={ 1.0, 2.0, 0.0},
       .orientation={0.0, 0.0, 0.0},
-      .velocity={-1.0, 0.0, 0.0},
+      .velocity={0.0, -1.0, 0.0},
       .angular_velocity={0.0, 0.0, 0.0},
       .force={0.0, 0.0, 0.0},
       .torque={0.0, 0.0, 0.0}});
