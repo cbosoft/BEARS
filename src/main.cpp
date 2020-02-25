40fda4cfc52ebfbf1b8c86c5808e59728c63d691
Christopher Boyle <chrismjboyle@gmail.com>
2020-02-25 15:54:46 +0000

    Added output trajectory to constructor and args

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,26 @@ int main(int argc, const char **argv)
 
   struct {
     std::string config_file_path;
+    std::string output_file_path;
     double end_time;
   } args = {
-    "config.ssv", 
-    10.0
+    "config.tsv", 
+    "traj.yaml",
+    1.0
   };
 
   // TODO: improve arg parsing
-  // TODO: check given path exists and is an ssv
+  // TODO: check given path exists and is in correct format
   --argc; ++argv;
   for (int i = 0; i < argc; i++) {
     if (EITHER("-c", "--config-file")) {
       args.config_file_path = std::string(argv[++i]);
+    }
+    else if (EITHER("-i", "--input-file")) {
+      args.config_file_path = std::string(argv[++i]);
+    }
+    else if (EITHER("-o", "--output-file")) {
+      args.output_file_path = std::string(argv[++i]);
     }
     else if (EITHER("-t", "--end-time")) {
       args.end_time = atof(argv[++i]);
@@ -36,7 +44,7 @@ int main(int argc, const char **argv)
   }
 
   std::cerr << "reading config" << std::endl;
-  Sim sim(args.config_file_path);
+  Sim sim(args.config_file_path, args.output_file_path);
 
   std::cerr << "begin!" << std::endl;
   sim.run(args.end_time);
