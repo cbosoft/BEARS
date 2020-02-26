dc14aadab924541d020d077cf65c5ecbece41f63
Chris <chrismjboyle@gmail.com>
2020-02-26 14:19:13 +0000

    Fixing yaml output

--- src/sim_io.cpp
+++ src/sim_io.cpp
@@ -35,9 +35,9 @@ void Sim::append_to_trajectory() const
   // of << " - " << this->time << std::endl;
 
   of << " - step:" << std::endl
-     << "     time: " << this->time << std::endl
+     << "   time: " << this->time << std::endl
   //    << "     number: " << this->balls.size() << std::endl
-     << "     balls: " << std::endl;
+     << "   balls: " << std::endl;
 
   for (auto ball: this->balls) {
     of << ball->to_yaml(5) << std::endl;
