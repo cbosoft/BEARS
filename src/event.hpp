a985bd43953e5a7a175f72cdac42c0576977f60e
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-20 20:47:26 +0000

    Added EventCollection type to contain events and manage them

--- src/event.hpp
+++ src/event.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <list>
+#include <map>
+#include <set>
 
 #include "vec.hpp"
 
@@ -45,35 +47,96 @@ class CollisionEvent {
     }
 };
 
-class EventCollection {
-
-  private:
-
-    std::list<CollisionEvent *> events;
-
-  public:
-
-    EventCollection() { }
-
-    void add_event(CollisionEvent *event)
-    {
-      if (this->events.size() and (event->get_time() < events.front()->get_time())) {
-        this->events.push_front(event);
-      }
-      else {
-        this->events.push_back(event);
-      }
-    }
-
-    const std::list<CollisionEvent *> &get_list() const
-    {
-      return this->events;
-    }
-};
-
 
 inline bool event_compare_f(const CollisionEvent *a, const CollisionEvent *b)
 {
   return a->get_time() < b->get_time();
 }
+
+
+class EventCollection {
+
+  private:
+
+    std::map<std::pair<int, int>, CollisionEvent *> events;
+    CollisionEvent *previous;
+
+
+  public:
+
+    EventCollection()
+    {
+      this->previous = NULL;
+    }
+
+    ~EventCollection() 
+    {
+      for (auto kv : this->events) {
+        delete kv.second;
+      }
+    }
+
+    void push_back(CollisionEvent *event)
+    {
+      auto pair = event->get_idpair();
+      if (this->events.find(pair) != this->events.end())
+        delete this->events[pair];
+      this->events[pair] = event;
+    }
+
+    std::set<unsigned int> invalidate();
+
+    void merge(EventCollection &other)
+    {
+      this->events.merge(other.events);
+    }
+
+    std::list<CollisionEvent *> get_list() const
+    {
+      std::list<CollisionEvent *> rv;
+      for (auto kv : this->events) {
+        rv.push_back(kv.second);
+      }
+      return rv;
+    }
+
+    std::list<CollisionEvent *> get_sorted_list() const
+    {
+      std::list<CollisionEvent *> rv = this->get_list();
+      rv.sort(event_compare_f);
+      return rv;
+    }
+
+    CollisionEvent *front()
+    {
+      auto l = this->get_sorted_list();
+
+      // get next event
+      auto rv = l.front();
+
+      // remove from store
+      this->events.erase(this->events.find(rv->get_idpair()));
+
+      // get invalid indices
+      // TODO
+
+      // set value
+      this->previous = rv;
+
+      // return value
+      return rv;
+    }
+
+    CollisionEvent *get_previous()
+    {
+      return this->previous;
+    }
+
+    size_t size() const
+    {
+      return this->events.size();
+    }
+
+};
+
 
