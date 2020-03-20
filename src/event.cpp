80fd36a7494d93bcc7005d1d7e392fed2448efa6
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-20 20:50:37 +0000

    Added method to get invalid ball indices for recomputation

--- src/event.cpp
+++ src/event.cpp
@@ -14,3 +14,24 @@ std::pair<int, int> CollisionEvent::get_idpair() const
   }
 }
 
+std::set<unsigned int> EventCollection::invalidate()
+{
+  int aid = this->previous->get_a()->get_id();
+  int bid = this->previous->get_b()->get_id();
+  std::set<unsigned int> rv;
+
+  for (auto it = this->events.begin(); it != this->events.end(); it++) {
+    auto kv = *it;
+    auto idpair = kv.first;
+    int bigger, smaller;
+    bigger = idpair.first;
+    smaller = idpair.second;
+    if ((bigger == aid) || (bigger == bid) || (smaller == aid) || (smaller == bid)) {
+      this->events.erase(it++);
+      rv.insert(bigger);
+      rv.insert(smaller);
+    }
+  }
+
+  return rv;
+}
