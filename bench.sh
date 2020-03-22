a9d78be539441679334479c0d4260dbba78af94d
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-22 18:25:02 +0000

    benchmark different number of threads

--- bench.sh
+++ bench.sh
@@ -9,7 +9,10 @@ bench_fail() {
 make clean && make BEARS configgen || bench_fail "compile error"
 
 numbers="10 100 1000 2000 3000 5000 8000 10000 20000 30000"
+threads="1 2 3 4 5"
 for n in $numbers; do
   ./configgen -n "$n" -o "$n.tsv" || bench_fail "config error"
-  ./BEARS -i "$n.tsv" || bench_fail "runtime error"
+  for t in $threads; do
+    ./BEARS -i "$n.tsv" -n "$t" || bench_fail "runtime error"
+  done
 done
