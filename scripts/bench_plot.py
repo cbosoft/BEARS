d75b98f7b91a9867334b63a623c52750b0f6d5d2
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-22 17:21:06 +0000

    Added plotter for benchmarks

---
+++ scripts/bench_plot.py
@@ -0,0 +1,33 @@
+from matplotlib import pyplot as plt
+
+def read_bench():
+    with open("benchmark.csv") as f:
+        lines = f.readlines()
+    data = [line.split(',') for line in lines]
+    data = list(zip(*data))
+    branches = set(data[1])
+    rv = {b:{
+        'time':list(),
+        'branch':b,
+        'nthreads':list(),
+        'nparticles':list(),
+        'time_to_update':list()
+        } for b in branches}
+    data = list(zip(*data))
+    for time, branch, nthreads, nparticles, time_to_update in data:
+        rv[branch]['time'].append(float(time))
+        rv[branch]['nthreads'].append(int(nthreads))
+        rv[branch]['nparticles'].append(int(nparticles))
+        rv[branch]['time_to_update'].append(float(time_to_update))
+    return rv
+
+if __name__ == "__main__":
+    data = read_bench()
+
+    for branch in data:
+        plt.plot(data[branch]['nparticles'], 
+                data[branch]['time_to_update'], 'o')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.show()
+
