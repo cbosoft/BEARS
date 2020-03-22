4defcd8faf5f8d1e7f90b9da52e3dd619708e8d5
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-22 17:49:06 +0000

    Added error checking to benchmark script

--- bench.sh
+++ bench.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/bash
 
+bench_fail() {
+  echo "benchmark failed! $1"
+  exit 1
+}
+
 # create required configurations of particles
-make clean && make BEARS configgen || exit
+make clean && make BEARS configgen || bench_fail "compile error"
 
 numbers="10 100 1000 2000 3000 5000 8000 10000 20000 30000"
 for n in $numbers; do
-  ./configgen -n "$n" -o "$n.tsv"
-  ./BEARS -i "$n.tsv"
+  ./configgen -n "$n" -o "$n.tsv" || bench_fail "config error"
+  ./BEARS -i "$n.tsv" || bench_fail "runtime error"
 done
