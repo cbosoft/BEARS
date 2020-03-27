a304f162f1333aa4ec1d0bfb3dff3c20402ae31c
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-27 16:13:18 +0000

    Updated energy plot for new traj output

--- scripts/energy_plot.py
+++ scripts/energy_plot.py
@@ -2,27 +2,45 @@ import sys
 
 import numpy as np
 from matplotlib import pyplot as plt
-import yaml
 
-path = 'traj.yaml'
+path = 'traj.tsv'
 if len(sys.argv) > 1:
     path = sys.argv[1]
 
-with open(path) as yamlf:
-    data = yaml.safe_load(yamlf)
+with open(path) as f:
+    lines = f.readlines()
 
-av_kinetic_energy = list()
 time = list()
-n = len(data[0]['balls'])
-for step in data:
-    ke = 0.0
-    for i, ball in enumerate(step['balls']):
-        print(ball)
-        ke += ball['kinetic_energy']
-    av_kinetic_energy.append(ke/n)
-    time.append(step['time'])
+av_kinetic_energy = list()
+this_step_energy = list()
+for line in lines:
+    if line.startswith('BEARS') or line.startswith('id'):
+        continue
+    elif line.startswith('n:'):
+        __, n, __, L = line.split()
+        n = int(n)
+        L = float(L)
+    elif line.startswith('t='):
+        __, t, aid, __, bid = line.split()
+        t = float(t)
+        time.append(t)
+        if this_step_energy:
+            av_kinetic_energy.append(np.average(this_step_energy))
+            this_step_energy = list()
+        aid = int(aid)
+        bid = int(bid)
+        if aid > n: aid = -1
+        if bid > n: bid = -1
+    else:
+        ball_info = line.split()
+        kinetic_energy = float(ball_info[-1])
+        this_step_energy.append(kinetic_energy)
+if this_step_energy:
+    av_kinetic_energy.append(np.average(this_step_energy))
+
 
 plt.plot(time, av_kinetic_energy)
 plt.xlabel('Time, $t$')
 plt.ylabel('Average Kinetic Energy, $\overline{E_K}$')
+plt.ylim(bottom=0.0, top=max(av_kinetic_energy)*1.1)
 plt.savefig('kinetic_energy.pdf')
